a59b59d634be896f3267fe51415813d52a0ebbe6
Gulu Guluzade <mister.guluem25@gmail.com>
2023-04-19 03:32:22 +0400

    The header, header-inner and the slider were done.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        speed: 2000,
+        infinity: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnFocus: true,
+    })
+})
