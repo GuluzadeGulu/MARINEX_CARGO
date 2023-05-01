9375f12ffc55d5ae2df8766ff11c1ec8c3b3fdfd
Gulu Guluzade <mister.guluem25@gmail.com>
2023-05-01 22:18:19 +0400

    I added 6 section, I will add footer and some details

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ $(document).ready(function(){
         pauseOnFocus: true,
     })
 })
+
+$(document).ready(function(){
+    $('.second-slider').slick({
+        arrows: false,
+        dots: false,
+        speed: 2000,
+        slidesToShow: 6,
+        infinity: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnFocus: true,
+    })
+})
